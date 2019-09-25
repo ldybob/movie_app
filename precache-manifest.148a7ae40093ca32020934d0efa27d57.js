412e25fc98882df986da8c2dcf7c70979f6d6e5a
ldybob <ldybob@gmail.com>
2019-09-25 11:45:10 +0900

    Updates

---
+++ precache-manifest.148a7ae40093ca32020934d0efa27d57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31be07fdc03faa3d2cd73dfd31ebfa62",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "ff915b6e6568aa2b18d2",
+    "url": "/movie_app/static/css/main.31fbf8cb.chunk.css"
+  },
+  {
+    "revision": "dee1dfeb0ee4e5d3fc91",
+    "url": "/movie_app/static/js/2.a9a352c0.chunk.js"
+  },
+  {
+    "revision": "ff915b6e6568aa2b18d2",
+    "url": "/movie_app/static/js/main.d3fbd623.chunk.js"
+  },
+  {
+    "revision": "0a5c0ad77459cf3396ae",
+    "url": "/movie_app/static/js/runtime~main.ebbd9dda.js"
+  }
+]);
